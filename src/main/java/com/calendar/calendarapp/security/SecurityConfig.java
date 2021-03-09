47de60bf94a358fc07ee77c16ce8230e7633325b
Ngo <ngomalalibo@yahoo.com>
2021-03-09 23:11:45 +0100

    Deploy to Heroku

--- src/main/java/com/calendar/calendarapp/security/SecurityConfig.java
+++ src/main/java/com/calendar/calendarapp/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.calendar.calendarapp.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+// @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
     
@@ -12,10 +14,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     public void configure(HttpSecurity httpSecurity) throws Exception
     {
         httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
-                    .antMatchers("/", "/index", "/home").authenticated()
+                    .antMatchers("/", "/index", "/home").permitAll()
                     .anyRequest().authenticated()
                     .and()
-                    .oauth2Login().permitAll()
+                    .oauth2Login()/*.loginPage("/home")*/
+                    .defaultSuccessUrl("/welcome", true)
+                    .failureUrl("/home")
                     .and().
                             logout().logoutSuccessUrl("/");
     }
