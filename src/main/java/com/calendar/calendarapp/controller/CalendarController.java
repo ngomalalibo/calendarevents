16912d71b36055ac50adc4df96ae8bd3430f5368
Ngo <ngomalalibo@yahoo.com>
2021-03-11 00:04:54 +0100

    Deploy to Heroku

--- src/main/java/com/calendar/calendarapp/controller/CalendarController.java
+++ src/main/java/com/calendar/calendarapp/controller/CalendarController.java
@@ -1,5 +1,7 @@
 package com.calendar.calendarapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -7,22 +9,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class CalendarController
 {
+    private static String authorizationRequestBaseUri
+            = "oauth2/authorization";
+    Map<String, String> oauth2AuthenticationUrls
+            = new HashMap<>();
+    
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
     
     @GetMapping({"/", "'home"})
-    public String getHome()
+    public String getHome(Model model)
     {
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                                            .as(Iterable.class);
+        if (type != ResolvableType.NONE &&
+                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0]))
+        {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
         
+        clientRegistrations.forEach(registration ->
+                                            oauth2AuthenticationUrls.put(registration.getClientName(),
+                                                                         authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
         return "home";
     }
     
